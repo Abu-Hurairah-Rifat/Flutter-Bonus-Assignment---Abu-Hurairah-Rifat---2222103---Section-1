1e88db35e5b9da4287a8ed1b92d71ba35cd8286b
Abu-Hurairah-Rifat <147369255+Abu-Hurairah-Rifat@users.noreply.github.com>
2026-08-13 20:47:14 +0600

    1st commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:summer_iub_app/screens/home.dart';
 import 'package:summer_iub_app/state_management/coffee_state_management.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/models/coffee_records_model.dart
+++ lib/models/coffee_records_model.dart
@@ -1,16 +1,32 @@
+// To parse this JSON data, do
+//
+//     final coffeeRecordsModel = coffeeRecordsModelFromJson(jsonString);
+
+import 'dart:convert';
+
+CoffeeRecordsModel coffeeRecordsModelFromJson(String str) =>
+    CoffeeRecordsModel.fromJson(json.decode(str));
+
+String coffeeRecordsModelToJson(CoffeeRecordsModel data) =>
+    json.encode(data.toJson());
+
 class CoffeeRecordsModel {
-  final int id;
-  final String title;
-  final String des;
-  double? amount;
-  final DateTime date;
+  final String id;
+  final String name;
+  final double price;
 
   CoffeeRecordsModel({
     required this.id,
-    required this.title,
-    required this.des,
-    this.amount,
-    required this.date,
+    required this.name,
+    required this.price,
   });
 
+  factory CoffeeRecordsModel.fromJson(Map<String, dynamic> json) =>
+      CoffeeRecordsModel(
+        id: json["id"],
+        name: json["name"],
+        price: json["price"]?.toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {"id": id, "name": name, "price": price};
 }
